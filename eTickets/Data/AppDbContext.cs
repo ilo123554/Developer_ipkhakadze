8f876c721c9cf86dc660c27f2b08377064fed399
Ilia P <pkhakadze.ilia@kiu.edu.ge>
2023-04-06 23:36:33 +0400

    models

---
+++ eTickets/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace eTickets.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
